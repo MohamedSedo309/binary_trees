ceb7c8ea5be187ab1a4e57f633ba91def4515490
MohamedSedo309 <mohamedsedo1907@gmail.com>
2023-08-02 12:16:04 -0700

    ditinh header file

--- binary_trees.h
+++ binary_trees.h
@@ -23,8 +23,10 @@ struct binary_tree_s *right;
 
 typedef struct binary_tree_s binary_tree_t;
 
+/* Printing helper function */
+void binary_tree_print(const binary_tree_t *);
 
-
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
 
 #endif
