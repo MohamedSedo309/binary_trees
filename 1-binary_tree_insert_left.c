d07e530df2f6b898dfb57c9b1041e62a088af3d2
MohamedSedo309 <110174037+MohamedSedo309@users.noreply.github.com>
2023-08-03 04:23:54 +0300

    Update 1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -8,25 +8,26 @@
  *Return: a pointer to the created node
  */
 
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 binary_tree_t *new_node;
-new_node = malloc(sizeof(binary_tree_t));
+
+if (parent == NULL)
+{
+return (NULL);
+}
+
+new_node = binary_tree_node(parent, value);
 if (new_node == NULL)
 {
 return (NULL);
 }
-new_node->n = value;
-new_node->parent = parent;
-if (parent->left == NULL)
-{
-new_node->left = NULL;
-new_node->right = NULL;
-}
-else
+
+if (parent->left != NULL)
 {
 new_node->left = parent->left;
-parent->left = new_node;
+parent->left->parent = new_node;
 }
+parent->left = new_node;
 return (new_node);
 }
